d6a8d2404e1acea5fad8d59efbea3fa5ec5740b4
able c j <gracyebin7@gamil.com>
2024-06-09 00:03:06 +0530

    deploying check completed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/your-repo-name/',
   plugins: [react()],
   build: {
     outDir: 'dist'
