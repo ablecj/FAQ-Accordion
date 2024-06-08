e17e133cc31add7c99bb3dbf789ad85387c6d918
able c j <gracyebin7@gamil.com>
2024-06-09 00:08:29 +0530

    message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/your-repo-name/',
+  base: '/FAQ-Accordion/',
   plugins: [react()],
   build: {
     outDir: 'dist'
